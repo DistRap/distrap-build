dc74bcfd6a5bd86fcf386be326522582979f74e8
Richard Marko <srk@48.io>
2017-10-27 18:50:45 +0200

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -4,5 +4,5 @@ with (import <nixpkgs> {});
 haskell.lib.buildStackProject {
   inherit ghc;
   name = "myEnv";
-  buildInputs = [ zlib x11 ] ++ (with xorg; [ libX11 libXcursor libXext libXfixes libXi libXinerama libXrandr libXxf86vm mesa ]);
+  buildInputs = [ git zlib x11 ] ++ (with xorg; [ libX11 libXcursor libXext libXfixes libXi libXinerama libXrandr libXxf86vm mesa ]);
 }
